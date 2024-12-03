b463d487149b81c2b2c87f74b9cea60c84999191
Jason Heilemann <traeh@aesthetics.tail312e8.ts.net>
2024-12-03 15:34:20 -0500

    Day 3 completed

--- day_3/instructions_management.py
+++ day_3/instructions_management.py
@@ -13,7 +13,10 @@ def create_python_tuples_from_input():
                 muls.append(((int(mul[4:mul.index(',')])), int(mul[mul.index(',') + 1:mul.index(')')])))
                 line = line.replace(mul, '')
         return muls
-    
+
+'''
+Prior iteration - worked but not as fancy
+
 def create_only_do_python_tuples_from_input():
     muls = []
     
@@ -40,7 +43,27 @@ def create_only_do_python_tuples_from_input():
                     num_2_start = mul_substring_candidate.index(',') + 1
                     num_2_end = mul_substring_candidate.index(')')
                     muls.append(((int(mul_substring_candidate[num_1_start:num_1_end])), int(mul_substring_candidate[num_2_start:num_2_end])))
-        return muls
+    return muls
+'''
+
+def create_only_do_python_tuples_from_input():
+    muls = []
+
+    with open('day_3/corrupted_instructions.txt', 'r') as f:
+        do_on = True
+        for line in f.readlines():
+            line = line.rstrip()
+            target_substrings = r"(do\(\)|don't\(\)|mul\([0-9]+,[0-9]+\))"
+            matches = re.findall(target_substrings, line)
+            for match in matches:
+                if match == "do()":
+                    do_on = True
+                elif match == "don't()":
+                    do_on = False
+                elif do_on:
+                    muls.append((int(match[4:match.index(',')]), int(match[match.index(',') + 1:match.index(')')])))
+    
+    return muls
 
 if __name__ == '__main__':
     #muls = create_python_tuples_from_input()
