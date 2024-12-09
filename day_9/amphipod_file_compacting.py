f37d1e7b5eb7484f5df47b5b693fb959d462b734
Jason Heilemann <traeh@aesthetics.tail312e8.ts.net>
2024-12-09 01:39:24 -0500

    Day 10 in progress

--- day_9/amphipod_file_compacting.py
+++ day_9/amphipod_file_compacting.py
@@ -6,22 +6,16 @@ if __name__ == '__main__':
     free_spaces = [disk_map[i] for i in range(1, len(disk_map), 2)]
     file_lengths = [disk_map[i] for i in range(0, len(disk_map), 2)]
     for i in range(len(free_spaces)):
-        id = chr(i)
         for j in range(int(file_lengths[i])):
-            final_list.append(id)
+            final_list.append(i)
         for j in range(int(free_spaces[i])):
             final_list.append('.')
     for j in range(int(file_lengths[len(free_spaces)])):
-        final_list.append(chr(len(free_spaces)))
-    #print(final_list[:50])
-    #print(final_list[len(final_list) - 100:])
+        final_list.append(len(free_spaces))
     while '.' in final_list:
         item = final_list.pop()
         if item != '.':
             final_list[final_list.index('.')] = item
     for i in range(len(final_list)):
-        #print(final_list[i])
-        #print(i * ord(final_list[i]))
-        checksum += i * ord(final_list[i])
-    #print(final_list[len(final_list) - 50:])
+        checksum += i * final_list[i]
     print(checksum)
