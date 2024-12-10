61abbb041628948fee40f962b0e19edfce27d03b
Jason Heilemann <traeh@aesthetics.tail312e8.ts.net>
2024-12-09 22:54:22 -0500

    Day 9 complete

--- day_9/amphipod_file_compacting_2.py
+++ day_9/amphipod_file_compacting_2.py
@@ -1,39 +1,69 @@
 if __name__ == '__main__':
     with open('day_9/disk_map.txt', 'r') as f:
         disk_map = list(f.readlines()[0])
-    final_list = []
-    intermediate_list = []
-    ultimate_list = []
+    first_round = []
+    second_round = []
+    third_round = []
     checksum = 0
     free_spaces = [disk_map[i] for i in range(1, len(disk_map), 2)]
     file_lengths = [disk_map[i] for i in range(0, len(disk_map), 2)]
     for i in range(len(free_spaces)):
         for j in range(int(file_lengths[i])):
-            final_list.append(i)
+            first_round.append(i)
         for j in range(int(free_spaces[i])):
-            final_list.append('.')
+            first_round.append('.')
     for j in range(int(file_lengths[len(free_spaces)])):
-        final_list.append(len(free_spaces))
-    print(final_list[len(final_list) - 50:])
-    for i in range(len(final_list) - 1, -1, -1):
-        if final_list[i] in intermediate_list or intermediate_list == []:
-            intermediate_list.append(final_list[i])
+        first_round.append(len(free_spaces))
+    for i in range(len(first_round) - 1, -1, -1):
+        if first_round[i] in second_round or second_round == []:
+            second_round.append(first_round[i])
         else:
-            ultimate_list.insert(0, intermediate_list)
-            intermediate_list = [final_list[i]]
-    ultimate_list.insert(0, intermediate_list)
-    for i in range(len(ultimate_list) - 1, -1, -1):
-        target_length = len(ultimate_list[i])
-        for j in range(i):
-            if len(ultimate_list[j]) == target_length and '.' in ultimate_list[j]:
-                ultimate_list.pop(j)
-                target_list = ultimate_list.pop()
-                ultimate_list.insert(j, target_list)
-
-
-
-    '''
-    for i in range(len(final_list)):
-        checksum += i * final_list[i]
+            third_round.insert(0, second_round)
+            second_round = [first_round[i]]
+    third_round.insert(0, second_round)
+    iterator = len(third_round) - 1
+    file_block = []
+    while 0 not in file_block:
+        file_block = third_round.pop(iterator)
+        target_length = len(file_block)
+        found_empty_space = False
+        if '.' in file_block:
+            third_round.insert(iterator, file_block)
+            iterator -= 1
+            continue
+        for j in range(iterator):
+            if len(third_round[j]) >= target_length and '.' in third_round[j]:
+                found_empty_space = True
+                empty_space = third_round.pop(j)
+                empty_space_size = len(empty_space)
+                if empty_space_size == target_length:
+                    third_round.insert(j, file_block)
+                else:
+                    for k in range(target_length):
+                        empty_space.remove('.')
+                    third_round.insert(j, empty_space)
+                    third_round.insert(j, file_block)
+                    iterator += 1
+                third_round.insert(iterator, ['.' for i in range(target_length)])
+                if '.' in third_round[iterator - 1]:
+                    grab = third_round.pop(iterator)
+                    third_round[iterator - 1] += grab
+                    iterator -= 1
+                    if iterator + 1 in range(len(third_round)) and '.' in third_round[iterator + 1]:
+                        grab = third_round.pop(iterator)
+                        third_round[iterator] += grab
+                if iterator + 1 in range(len(third_round)) and '.' in third_round[iterator + 1]:
+                    grab = third_round.pop(iterator)
+                    third_round[iterator] += grab
+                break
+        if not found_empty_space:
+            third_round.insert(iterator, file_block)
+        iterator -= 1
+    ultimate_round = []
+    for item in third_round:
+        for subitem in item:
+            ultimate_round.append(subitem)
+    for i in range(len(ultimate_round)):
+        if ultimate_round[i] != '.':
+            checksum += i * ultimate_round[i]
     print(checksum)
-    '''
